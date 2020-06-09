987a4dd08b2c3bada42039ba48d5365596a998c6
澤田こうき <sawadakouki@mbp.local>
2020-06-09 10:09:01 +0900

    いいね・コメント機能非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -7,15 +7,20 @@ class BookCommentsController < ApplicationController
 		@book_comment.user_id = current_user.id
 		@bbook = Book.new
 		@book_comments = @book.book_comments
-		@book_comment.save
+		if @book_comment.save
+			render :index
+		end
 	end
 
 	def destroy
-		@book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+		@book = Book.find(params[:book_id])
+		@book_comment = BookComment.find_by(id: params[:id], book_id: @book.id)
+		@book_comments = @book.book_comments
 		if @book_comment.user != current_user
 			redirect_to book_path(params[:book_id])
 		end
 		@book_comment.destroy
+		render :index
 	end
 
 private
